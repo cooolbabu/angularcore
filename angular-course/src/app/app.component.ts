a54d067c4027ce83d463aa296902cf6d9a9b41ab
Sreenivas Angara <cooolbabu@yahoo.com>
2023-07-29 13:19:00 -0500

    Complete Section 2

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {COURSES} from '../data/datadb'
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sreenivas welcome to Angular deep dive course';
+  price = 9.99;
+  rate = .30;
+
+  courses = COURSES;
+
+  startDate = new Date(2000, 0, 1);
+
+  onCourseSelected(courseEvent:Course) {
+    console.log("Card clicked ... ", courseEvent)
+  }
 }
